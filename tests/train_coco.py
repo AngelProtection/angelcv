ac049da646aa69f2e1bb01504337ace3eed697c1
Iu Ayala <iu.ayala@gradientinsight.com>
2025-06-26 08:50:16 +0100

    Change the parameters for production and debug mode for train_coco.py

--- tests/train_coco.py
+++ tests/train_coco.py
@@ -1,14 +1,32 @@
 from angelcv import ObjectDetectionModel
+from angelcv.utils.env_utils import is_debug_mode
 
 model = ObjectDetectionModel("yolov10m.yaml")
 
-train_result = model.train(
-    dataset="coco.yaml",
-    max_epochs=900,
-    patience=50,
-    image_size=640,
-    batch_size=8,
-    devices=-1,  # all devices
-)
+if is_debug_mode():
+    print("Running in DEBUG mode")
+    print("IMPORTANT: NOT Training the entire dataset, overfit_batches=64")
+    train_result = model.train(
+        dataset="coco.yaml",
+        max_epochs=900,
+        patience=50,
+        image_size=640,
+        batch_size=1,
+        devices=-1,  # all devices
+        overfit_batches=64,  # NOT Training the entire dataset
+        num_workers=1,
+    )
+else:
+    print("Running in PRODUCTION mode")
+    train_result = model.train(
+        dataset="coco.yaml",
+        max_epochs=900,
+        patience=50,
+        image_size=640,
+        batch_size=32,
+        devices=-1,  # all devices
+        num_workers=-1,
+    )
+
 
 print(train_result)
